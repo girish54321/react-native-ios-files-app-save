7f1d5f66569a798c60ce349e9121a2db0b667a98
Girish Parate <girishparate@Girishs-iMac.local>
2023-02-11 21:46:21 +0530

    Update Redme

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,12 +1,54 @@
 import * as React from 'react';
 
-import { StyleSheet, View, Text } from 'react-native';
+import { StyleSheet, View, Text, FlatList, TouchableOpacity, Image, SafeAreaView } from 'react-native';
 import { stateDownloadAppSave } from 'react-native-ios-files-app-save';
 
+const fileArray = [
+  {
+    title: "PDF",
+    image: require('./images/pdf.png'),
+    url: "http://www.africau.edu/images/default/sample.pdf"
+  },
+  {
+    title: "PNG",
+    image: require('./images/png.png'),
+    url: "https://file-examples-com.github.io/uploads/2017/10/file_example_PNG_1MB.png"
+  },
+  {
+    title: "ZIP",
+    image: require('./images/zip.png'),
+    url: "https://www.learningcontainer.com/wp-content/uploads/2020/05/sample-large-zip-file.zip"
+  },
+  {
+    title: "JPG",
+    image: require('./images/jpg.png'),
+    url: "https://file-examples-com.github.io/uploads/2017/10/file_example_JPG_1MB.jpg"
+  },
+  {
+    title: "DOC",
+    image: require('./images/doc.png'),
+    url: "https://file-examples-com.github.io/uploads/2017/02/file-sample_1MB.doc"
+  }
+]
 export default function App() {
 
-  const demoDownload = () => {
-    stateDownloadAppSave("https://unec.edu.az/application/uploads/2014/12/pdf-sample.pdf").then((res: any) => {
+
+  const renderItem = ({ item }) => {
+    return (
+      <View style={{ flex: 0.5, padding: 22, }} >
+
+        <TouchableOpacity onPress={() => {
+          demoDownload(item.url);
+        }} >
+          <Image source={item.image} style={{ height: 160, width: 160, padding: 22 }} />
+        </TouchableOpacity>
+        <Text style={{ alignSelf: 'center', marginTop: 16, fontSize: 22 }}>{item.title}</Text>
+      </View>
+    );
+  };
+
+  const demoDownload = (url: string) => {
+    stateDownloadAppSave(url).then((res: any) => {
       console.log(res);
     }).catch((e) => {
       console.log("error", e);
@@ -14,17 +56,21 @@ export default function App() {
   }
 
   return (
-    <View style={styles.container}>
-      <Text onPress={demoDownload}>{"Start Download"}</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.container}>
+        <FlatList
+          data={fileArray}
+          renderItem={renderItem}
+          numColumns={2}
+        />
+      </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
   },
   box: {
     width: 60,
